9eefa9d3299cb2366eb283363108669e547999f1
Bulychev Nick <nick@kescom.com>
2018-05-23 16:02:52 +0300

    Первичная заливка репозитория

---
+++ Accessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+
+namespace Kesco.Lib.Localization
+{
+    /// <summary>
+    /// Класс содержащий статический объект менеджер ресурсов
+    /// </summary>
+    public class Resources
+    {
+        /// <summary>
+        /// Статический менеджер ресурсов
+        /// </summary>
+        public static ResourceManager Resx
+        {
+            get { return new ResourceManager(typeof (ResContainer)); }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Kesco.Lib.Log;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Kesco.Lib.Localization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kesco-M")]
+[assembly: AssemblyProduct("Kesco.Lib.Localization")]
+[assembly: AssemblyCopyright("Copyright © Kesco-M")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyIsKesco(true)]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a4e726b7-2756-442e-a51b-34365f792e11")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.*")]
